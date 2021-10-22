57326a9afa984465b2aece767d0e1979093e2161
Umer Arif <62794724+umerarif01@users.noreply.github.com>
2021-10-22 15:58:12 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -13,22 +13,22 @@ function MyApp({ Component, pageProps }) {
      </div>
      <div className="flex mt-4">
           <Link href="/">
-            <a className="mr-6 text-purple-500 font-bold hover:text-white hover:bg-gradient-to-r from-blue-600 to-purple-600 text-white rounded-xl p-2 transition ">
+            <a className="mr-6 text-blue-500 font-bold hover:text-white hover:bg-gradient-to-r from-blue-600 to-purple-600 text-white rounded-xl p-2 transition ">
               Home
             </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-purple-500 font-bold hover:text-white hover:bg-gradient-to-r from-blue-600 to-purple-600 text-white rounded-xl p-2 transition">
+            <a className="mr-6 text-blue-500 font-bold hover:text-white hover:bg-gradient-to-r from-blue-600 to-purple-600 text-white rounded-xl p-2 transition">
               Sell Digital Asset
             </a>
           </Link>
           <Link href="/my-assets">
-            <a className="mr-6 text-purple-500 font-bold hover:text-white hover:bg-gradient-to-r from-blue-600 to-purple-600 text-white rounded-xl p-2 transition">
+            <a className="mr-6 text-blue-500 font-bold hover:text-white hover:bg-gradient-to-r from-blue-600 to-purple-600 text-white rounded-xl p-2 transition">
               My Digital Assets
             </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-purple-500 font-bold hover:text-white hover:bg-gradient-to-r from-blue-600 to-purple-600 text-white rounded-xl p-2 transition">
+            <a className="mr-6 text-blue-500 font-bold hover:text-white hover:bg-gradient-to-r from-blue-600 to-purple-600 text-white rounded-xl p-2 transition">
               Creator Dashboard
             </a>
           </Link>
